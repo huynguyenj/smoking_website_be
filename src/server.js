72e7ba73c856c0dad5d1ba3a782609adb55f1535
huynguyenj <nguyenhuyjobs@gmail.com>
2025-05-18 21:44:22 +0700

    update router-v1

--- src/server.js
+++ src/server.js
@@ -3,16 +3,14 @@ import express from 'express'
 import { CLOSE_DB, CONNECT_DB } from '@/config/mongodb'
 import exitHook from 'async-exit-hook'
 import { env } from '@/config/environment'
-
+import { APIs_V1 } from '@/routes/v1'
 //Tất cả config phải nằm trong này nếu để ngoài sẽ xảy ra tình trạng bất đồng bộ khi kết nối database cần thời gian để kết nối nhưng hàm khác lại chạy trc.
 const START_SERVER = () => {
   const app = express()
   const hostname = 'localhost'
   const PORT = env.APP_PORT
 
-  app.get('/', ( req, res ) => {
-    res.send('Hello world!')
-  })
+  app.use('/v1', APIs_V1)
 
   app.listen(PORT, async () => {
     console.log(`Back-end server is running at host: ${hostname} at PORT: ${PORT}`)
