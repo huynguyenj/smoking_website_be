0dd88870f473f76981ef4e12866df23b2670d8ff
huynguyenj <nguyenhuyjobs@gmail.com>
2025-07-14 15:27:08 +0700

    fix origin in  socket

--- src/providers/socketio.js
+++ src/providers/socketio.js
@@ -1,7 +1,6 @@
-import { env } from '@/config/environment'
 import { Server } from 'socket.io'
 
 export const connectSocket = async (httpServer) => {
-  const io = new Server(httpServer, { cors:{ origin: env.BUILD_MODE === 'production' ? env.CLIENT_URL_PROD2 : '*' } })
+  const io = new Server(httpServer, { cors:{ origin: '*' } })
   return io
 }
